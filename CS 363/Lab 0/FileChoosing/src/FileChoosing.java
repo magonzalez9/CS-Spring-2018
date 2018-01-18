a20db3dd84eb4bfbb9e7714590d4445c9bbc7d00
magonzalez9 <magonzalez@willamette.edu>
2018-01-17 22:44:10 -0800

    Update to lab 3

--- CS 363/Lab 0/FileChoosing/src/FileChoosing.java
+++ CS 363/Lab 0/FileChoosing/src/FileChoosing.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -6,40 +7,50 @@ import javax.swing.JFrame;
  * @author Marco
  */
 public class FileChoosing {
-    
+
     public static void main(String[] args) {
-        
+
 //        File f = new File("Source Packages/");
 //        String currentDirectory = f.getAbsolutePath();
 //        System.out.println("currentDirectory = " + currentDirectory);
-        
-       new FileChoosing().printFilesInADirectorySelectedByTheUser();
+        new FileChoosing().printFilesInADirectorySelectedByTheUser();
     }
-        
-    void printFilesInADirectorySelectedByTheUser() {        
-        JFileChooser fc = new JFileChooser(); 
+
+    void printFilesInADirectorySelectedByTheUser() {
+        JFileChooser fc = new JFileChooser();
         fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        
+
         // Prompt for file selection
         int returnVal = fc.showOpenDialog(new JFrame());
-        
+
         // Check for errors
-        if (returnVal != JFileChooser.APPROVE_OPTION) {  
+        if (returnVal != JFileChooser.APPROVE_OPTION) {
             System.out.println("Error");
             return;  // give up, go home
         }
-        
-        File f = fc.getSelectedFile();
 
+        File f = fc.getSelectedFile();
         System.out.println("You selected the directory = " + f);
+        System.out.println(f);
+
+        // Print contents
         printContentsOfDirectory(f);
     }
-    
+
     void printContentsOfDirectory(File f) {
+
         for (File nextFile : f.listFiles()) {
-            System.out.println("\tnextFile = " + nextFile);
-            System.out.println("PATH: "+nextFile.getAbsolutePath());
+                   
+            System.out.println(nextFile);
+
+            // Print recoursively
+            if (nextFile.isDirectory() && nextFile.listFiles() != null) {
+
+               
+
+                printContentsOfDirectory(nextFile);
+            }
         }
     }
-    
+
 }
