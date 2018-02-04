bb7e8bda30de697aa4881f1d8dd7b70bfe014e09
magonzalez9 <magonzalez@willamette.edu>
2018-02-03 18:34:02 -0800

    Lab 2 complete

--- CS 363/Lab 2/LV_Proto/src/lv_proto/DataPairList.java
+++ CS 363/Lab 2/LV_Proto/src/lv_proto/DataPairList.java
@@ -55,7 +55,7 @@ class DataPairList extends ArrayList<DataPair> {
     }
 
     public int getpMax() {
-        
+         
         return pMax;
     }
 
--- CS 363/Lab 2/LV_Proto/src/lv_proto/Phase.java
+++ CS 363/Lab 2/LV_Proto/src/lv_proto/Phase.java
@@ -18,15 +18,15 @@ public class Phase extends javax.swing.JPanel {
 
     DataPairList dp_list;
     int yOffset = 10, left, top, rows, right, bottom;
-    int pMax, pMin, hMax, hMin, pairSize, hRange, pRange, graphRange;
-    
+    int pMin, pMax, hMax, hMin, pairSize, hRange, pRange, yGraphRange, xGraphRange;
+
     public Phase() {
         initComponents();
         setLayout(null);
         setVisible(true);
         setPreferredSize(new Dimension(2000, 2000));
     }
-    
+
     @Override
     public void paintComponent(Graphics g) {
         if (g == null) {
@@ -42,66 +42,79 @@ public class Phase extends javax.swing.JPanel {
 
     public void paintAxis(Graphics g) {
         Rectangle r = this.getVisibleRect();
-        graphRange = bottom - top;
+        yGraphRange = bottom - top;
+        xGraphRange = right - left;
+
+        System.out.println("YRange: " + yGraphRange);
+        System.out.println("XRange " + xGraphRange);
+
         // Set axis points
         left = r.x + 40;
-        top = (r.y + valueToPixels(hMin) + yOffset);
-        right = left + r.width + valueToPixels(dp_list.size()); //valueToPixels
-        bottom = top + r.height - 20;
+        top = (r.y + yOffset);
+        right = r.width - (top + 20); //valueToPixels
+        bottom = top + r.height - 30;
 
         // Draw the axis
         g.drawLine(left, bottom, right, bottom); // x Axis
         g.drawLine(left, top, left, bottom); // y Axis
         g.drawLine(left, top, right, top); // top x Axis
     }
-    
-    public void paintXAxis(){
+
+    public void paintXAxis() {
         Rectangle r = this.getVisibleRect();
-        
+
     }
 
     public void drawTicks(Graphics g) {
         int hInc = hRange / 5;
-        int pY = hMax;
+        int hY = hMax;
         int pixelY;
         String tickValue;
 
-        // Draw ticks and their values
+        // Draw ticks and their values for H
         for (int y = hMin; y <= hMax; y += hInc) {
-            pixelY = valueToPixels(pY) + yOffset;
+            pixelY = valueToPixels(hY, 'y') + yOffset;
             tickValue = "" + y;
             g.drawLine(30, pixelY, left, pixelY);
             g.drawString(tickValue, left - 40, pixelY);
-            pY -= hInc;
+            hY -= hInc;
+        }
+        int pInc = pRange / 5;
+        int pX = pMax;
+        int pixelX;
+        String pValue;
+        System.out.println(dp_list);
+        // Draw ticks and their values for P
+        for (int y = pMax; y >= pMin; y -= pInc) {
+            pixelX = valueToPixels(pX, 'x') + left;
+            pValue = "" + y;
+            g.drawLine(pixelX, valueToPixels(hMax, 'y') + yOffset, pixelX, valueToPixels(hMax, 'y') + 15);
+            g.drawString(pValue, pixelX - 10, valueToPixels(hMax, 'y') + 25);
+            pX -= pInc;
         }
     }
 
-    public int valueToPixels(int input) {
-        return (input - hMin) * ((graphRange)) / (hRange);
+    public int valueToPixels(int input, char axis) {
+        if (axis == 'y') {
+            return (input - hMin) * ((yGraphRange) - 0) / (hRange - 0) + 0;
+        } else {
+            return (input - pMin) * ((xGraphRange) - 0) / (pRange - 0) + 0;
+        }
     }
 
     public void paintCircles(Graphics g) {
-        int xTraverse = left;
-        int pY;
-        int hY;
-
         int hGraphValue;
-        int pGraphValue; 
+        int pX;
+        int hY;
 
         for (DataPair dataPair : dp_list) {
             hGraphValue = hMax - dataPair.h;
-            pGraphValue = hMax - dataPair.p; 
-            
-            g.setColor(Color.RED);
-            pY = valueToPixels(pGraphValue)+ yOffset;
-            g.drawOval(xTraverse, pY, 5, 5);
+            pX = valueToPixels(dataPair.p, 'x') + left;
+            hY = valueToPixels(hGraphValue, 'y') + yOffset;
 
-            g.setColor(Color.GREEN);
-            hY = valueToPixels((hGraphValue) + yOffset);
-            g.drawOval(xTraverse, hY, 5, 5);
-            
-            // Move 5 to the right
-            xTraverse += 2;
+            g.setColor(Color.RED);
+            g.drawOval(pX, hY, 5, 5);
+
         }
 
         System.out.println(dp_list);
@@ -110,21 +123,19 @@ public class Phase extends javax.swing.JPanel {
 
     public void setValues() {
         dp_list.getMax();
-        dp_list.getMin(); 
+        dp_list.getMin();
         pairSize = dp_list.size();
-        pMax = dp_list.getpMax(); 
+        pMax = dp_list.getpMax();
         pMin = dp_list.getpMin();
         hMax = dp_list.gethMax();
         hMin = dp_list.gethMin();
         hRange = hMax - hMin;
-        pRange = pMax = pMin; 
-        
+        pRange = pMax - pMin;
+
         System.out.println("hMax: " + hMax);
         System.out.println("hMin: " + hMin);
-        
         System.out.println("pMax: " + pMax);
         System.out.println("pMin: " + pMin);
-        System.out.println(dp_list);
 
     }
 
@@ -134,8 +145,8 @@ public class Phase extends javax.swing.JPanel {
 
         setLayout(null);
     }// </editor-fold>//GEN-END:initComponents
-    void go(DataPairList list){
-        this.dp_list = list; 
+    void go(DataPairList list) {
+        this.dp_list = list;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
